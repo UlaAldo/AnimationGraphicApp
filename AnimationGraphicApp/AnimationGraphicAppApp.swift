7e3d5d4386806f2f03a787a7c6560dfe5c1c8210
Ula <1295593@mail.ru>
2022-04-23 11:27:23 +0800

    Initial Commit

---
+++ AnimationGraphicApp/AnimationGraphicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationGraphicAppApp.swift
+//  AnimationGraphicApp
+//
+//  Created by Юлия Алдохина on 23/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationGraphicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
